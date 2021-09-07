32f84c5638f60bfe83368c070ecf1dc4836dec02
booklover4177 <anne.straub@gmail.com>
2021-09-07 12:12:44 -0400

    setup routing and created home and episode-list components.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from "@angular/core";
+import { CommonModule} from "@angular/common";
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./components/home/home.component";
+
+
+const routes = [
+  {path: '', component:HomeComponent}
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './components/app/app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
